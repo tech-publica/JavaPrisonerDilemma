9d100cd0c7bc212c9d50701767c370d77440498b
tech.publica <tech.publica@gmail.com>
2023-02-14 10:23:25 +0100

    corregge modifica di esempio nel giocatore BastardInside

--- src/main/java/com/example/prisoner/players/BastardInside.java
+++ src/main/java/com/example/prisoner/players/BastardInside.java
@@ -3,7 +3,7 @@ package com.example.prisoner.players;
 import java.util.ArrayList;
 
 public class BastardInside implements Player{
-    private ArrayList<Move> moves = new ArrayList<>();
+
     private int score;
     public int getScore() {
         return score;
@@ -11,17 +11,11 @@ public class BastardInside implements Player{
 
     public Move nextMove() {
     	
-    	Move first = moves.get(0);
-    	Move last = moves.get(moves.size()-1);
-    	for(int i = 0; i < moves.size(); i++) {
-    		Move m = moves.get(i);
-    	}
-    	
         return Move.DEFECT;
     }
 
     public void receiveOpponentMove(Move move) {
-        moves.add(move);
+
     }
 
     public void addScore(Score score) {
@@ -29,11 +23,11 @@ public class BastardInside implements Player{
     }
 
     public void onChangeOpponent() {
-        moves.clear();
+
     }
 
     public void onReset() {
         this.score = 0;
-        moves.clear();
+
     }
 }
